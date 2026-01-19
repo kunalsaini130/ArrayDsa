81ac27aaedf07df2b91264c06552857e13dd8bfc
kunal saini <sainikunal605@gmail.com>
2026-01-19 10:40:47 +0530

    dfk

--- ArrayInde.java
+++ ArrayInde.java
@@ -1,15 +1,20 @@
  public class ArrayInde {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4};
-        int x = 3;
+        int x = 4;
+        boolean found=false;
         for(int i = 0; i < arr.length; i++)
         {
             
          if(arr[i] == x)
             {
                 System.out.print("found index :" +i);
+                found=true;
                 break;
             }
+        }
+        if(!found){
+            System.out.println("not found");
         }
         
     
--- UserInputArray.java
+++ UserInputArray.java
@@ -9,6 +9,7 @@ public class UserInputArray {
         int arr []=new int[n];
 
         System.out.println("Enter the element : ");
+        
 
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
